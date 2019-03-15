d6bafe15127416ff9a148efcae8578d0675053b9
Nithiyaa Radjindirin <Nithiyaa.Radjindirin@vonessenbank.de>
2019-03-15 09:07:57 +0100

    added junit dependencies to pom & junit tests

--- src/test/java/org/vebqa/vebtal/td/commands/CreateibanTest.java
+++ src/test/java/org/vebqa/vebtal/td/commands/CreateibanTest.java
@@ -1,5 +1,24 @@
 package org.vebqa.vebtal.td.commands;
 
+import static org.hamcrest.beans.SamePropertyValuesAs.samePropertyValuesAs;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.vebqa.vebtal.model.Response;
+
 public class CreateibanTest {
+
+	@Test
+	public void generateIBAN() {
+		Createiban cmd = new Createiban("createIBAN", "", "");
+		
+		Response actualResult = cmd.executeImpl(new Object());
+
+		Response expectedResult = new Response();
+		expectedResult.setCode(Response.PASSED);
+		expectedResult.setMessage("");
+		
+		assertThat(expectedResult, samePropertyValuesAs(actualResult));
+	}
 
 }
